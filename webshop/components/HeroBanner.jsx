10fabb4fda405b25bbd65fb2ccb546688c53472b
Aziz <80921378+aziz-zai@users.noreply.github.com>
2022-12-10 15:08:57 +0100

    heroBanner data fetched from sanity

--- webshop/components/HeroBanner.jsx
+++ webshop/components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
 import React from "react";
+import { urlFor } from '../lib/client';
 
-function HeroBanner() {
+
+function HeroBanner({heroBanner}) {
   return (
   <div className="hero-banner-container">
     <div>
-        <p className="beats-solo">SMALL TEXT</p>
-    <h3>MID TEXT</h3>
-    <img src="" alt="product" className="hero-banner-image"></img>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+    <h3>{heroBanner.midText}</h3>
+    <h1>{heroBanner.largeText1}</h1>
+    <img src={urlFor(heroBanner.image)} alt="product" className="hero-banner-image"></img>
 
     <div>
-        <Link href="/product/ID">
-            <button type="button"> BUTTON TEXT</button>        
+        <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>        
         </Link>
         <div className="desc">
-            <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <h5>Description</h5> 
+            <p>{heroBanner.desc}</p>
         </div>
     </div>
     </div>          
